f6900f21b60af58619db77dd5e22513e129a4388
Jesse Burström <jesse.burstrom@gmail.com>
2022-06-27 10:36:57 +0200

    refactor

--- src/web/flutter_service_worker.js
+++ src/web/flutter_service_worker.js
@@ -52,7 +52,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2aa0716207d5bbf85c192257c8fd4bc4",
+"index.html": "6c8e96a995a656a8f87141cebc480e3f",
 "/": "638f767633094d26133d10a8957960d9",
 "main.dart.js": "8ad9ed51e2a2b5fbdedac54f35ca2bef",
 "manifest.json": "3c9c0a4c1427016b6e684093bc2b47f7",
